a00464d3ceb2ce7a1ca1da098ec53035557cfd43
John J Setlock II <johnsetlock@gmail.com>
2022-08-23 12:49:57 -0500

    Added 404 page

--- src/NotFound.js
+++ src/NotFound.js
@@ -3,7 +3,7 @@ function NotFound (){
     return (
         <div>
             <h1>404 Not Found</h1>
-            <img src="./notfound.jpg" alt="404 not found" />
+            <img src="./notfound.jpg" alt="404 not found " />
         </div>
     )
 }
